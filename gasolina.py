057603bfc7a709ad7eefc8cebb6431e35dfb7beb
LauraSabrinaOshiro <laura7oshiro@gmail.com>
2023-04-24 15:47:42 +0000

    titulo adicionado

--- gasolina.py
+++ gasolina.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 gasolina = pd.read_csv('./gasolina.csv')
 
 plt.plot(gasolina["dia"], gasolina["venda"])
+plt.title('Valor da venda por dia')
 plt.xlabel('dia')
 plt.ylabel('venda')
 plt.savefig('gasolina.png', format='png')
 plt.show()
+
